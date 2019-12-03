f5379d9a8720b7ed9b3257b51ffb0078fea1017b
chenbainian <284049379@qq.com>
2019-12-03 10:41:57 +0800

    hystrix

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -7,14 +7,14 @@ import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-@Accessors(chain = true)
+@Accessors(chain=true)
 public class Dept implements Serializable { //Dept(Entity) orm mysql -> Dept（table） 类表关系映射 // 必须序列化
 
     private Long deptno; //主键
-    private String dName;//部门名称
+    private String dname;//部门名称
     private String db_source;//来自哪个数据库，因为微服务架构可以一个服务对应一个数据库，同时一个信息被存储到不同数据库
 
 
--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -2,7 +2,6 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
